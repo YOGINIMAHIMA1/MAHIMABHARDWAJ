c954cb5ff7ae30ecbb88d8f654bcf916f43c69b7
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-07-20 11:39:20 +0530

    evenorodd.cpp
    number is even or odd

---
+++ evenorodd.cpp
@@ -0,0 +1,17 @@
+/*  find the number is even or odd using  c++ */
+#include <iostream>
+using namespace std;
+int main() {
+               int n;
+               cout<<"Enter the number:";
+               cin>>n;
+               if(n%2==0)
+               {
+               cout<<"Even no:";
+               }
+              else
+              {
+               cout<<"odd no:";
+               }
+               return 0;
+}
