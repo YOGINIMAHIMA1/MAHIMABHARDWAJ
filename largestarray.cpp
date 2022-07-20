77bc3d091fa96ad307b65ae3fef6c15e7d51fcd3
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-07-20 11:29:57 +0530

    Create largestarray.cpp
    how to find largest element in array

---
+++ largestarray.cpp
@@ -0,0 +1,15 @@
+*/ Enter largest element in an array */
+#include<iostream>
+using namespace std;
+int main(){
+                  int arr[10];
+                  int large ;
+                  arr[0]=large;
+                  for (int i=0;i<=9;i++)
+                  {
+                         if(arr[i]>arr[0])
+                         arr[i]=large;
+                         }
+                         cout<<"largest number is :"<<arr[i]<<endl;
+                 }
+                 
