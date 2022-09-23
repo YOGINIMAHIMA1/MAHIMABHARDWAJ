8ea4dc08e6feaa96e1c19bedc6b1d5052309b9c6
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-09-23 06:59:36 -0700

    Create relationaloperator.cpp

---
+++ relationaloperator.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a,b,c ,d,e;
+  cin>>a>>b>>c;
+  d=a<b<c;
+  e=a>b>c;
+  int f=d>=e<a;
+  int g=c<=f>=b;
+  cou<<a<<endl;
+  cout<<b<<endl;
+  cout<<c<<endl
+    cout<<d<<endl;
+  cout<<e<<endl;
+  cout<<g<<endl;
+  cout<<f<<endl;
+  return 0;
+}
