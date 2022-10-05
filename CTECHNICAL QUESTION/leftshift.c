259dad35615d7042d135a5b740cb2eb0a515aa80
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-10-04 22:07:24 -0700

    Update leftshift.c

--- CTECHNICAL QUESTION/leftshift.c
+++ CTECHNICAL QUESTION/leftshift.c
@@ -5,7 +5,7 @@ int main()
   
   clrscr();
   int a=-1;
-  printf("%x",x>>4);
+  printf("%x",x<<4);
   return 0;
 }
 /*  1>  as a is 1 so find out its binary value and stored in 16 bit as it is integer type.
@@ -17,7 +17,7 @@ int main()
  3> now , left shift value by 4 just delete 4 values from left side and shift other value at their place and add 0 value at right side where box are empty
   for ex-   1 1 1 1   1 1 1 1   1 1 1 1   1 1 1 1
         left shift by 4
-            1 1 1 1   1 1 1 1   1 1 1 1   0 0 0 0
+            1 1 1 1   1 1 1 1   1 1 1 1   0 0 0 0 //x<<4
             
    4>   now find out hexadecimal after left shift
            1 1 1 1   1 1 1 1  1 1 1 1  0 0 0 0
