85b862ff19eb194b5a14b4b6a9dbf99a49eb4bf8
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-10-04 21:36:03 -0700

    Create increment.c
    pre-increment operator is used in this program.
    increment operator does not used expression or constant as variable.
    rules for modify operator
    *  pre-increment/decrement
    * substitution
    * evaluation
    * post-increment/decrement

---
+++ CTECHNICAL QUESTION/increment.c
@@ -0,0 +1,26 @@
+/* For increment operator operand should be variable.
+   operand should not be expression or constant as variable it will show error. */
+ #include <stdio.h>
+  #include <conio.h>
+int main()
+{
+  clrscr();
+  int a=10 , b=20;
+  printf("%d",++(a*b+1));
+  return 0;
+}
+
+/*   op:compile time error */
+
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+  
+ 
